2bd73710a18923d190f7923a754ddfda8a82c669
KH3Group <kh3group@KH3Groups-iMac.local>
2021-06-27 11:36:59 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+fetch("footage.json")
+    .then(response => response.json())
+    .then(data => {
+        let html = '';
+        for (let item in data) {
+            html += 
+            `<div class="row">
+                <div class="col-details">
+                    <p><b>Date:</b> ${data[item]["date"]}</p>
+                    <p><b>Time:</b> ${data[item]["time"]}</p>
+                    <p><b>Source:</b> ${data[item]["source_file"]}.mp4</p>
+                </div>
+                <div class="col-image"><img class="img" src="${data[item]["url"]}" alt=""></div>
+            </div>`;
+        }
+        container.innerHTML += html;
+        console.log(html);
+    });
